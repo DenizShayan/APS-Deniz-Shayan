d3c2c4b7baf26a47cee168b0b53cf78cf3b9b7de
DenizShayan <deniz.shayan.91@gmail.com>
2022-01-11 11:52:25 +0330

    1.0
    Starting my daily coding. :)

---
+++ folder1/script.js
@@ -0,0 +1,11 @@
+for(let i=0; i<=6; i++){
+    console.log(`${i+1} -- in shabi ke migan shab nis. age shabe mesle dishab nis...`);
+}
+
+for(let i=0;i<=20;i+=2){
+    if(i%2 === 0){
+        console.log(`Number &{i}`);
+    }
+}
+
+let feelings=[];
